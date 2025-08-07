9335fc9a7740f48b12e71298bd51c1859a3989c1
ahmed-gameel12 <a.gameel101@gmail.com>
2025-08-07 18:30:09 +0300

    e

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ class RSSIInput(BaseModel):
 @app.post("/predict")
 def predict_location(data: RSSIInput):
     try:
+
+
         input_array = np.array([data.rssi], dtype=float)  # shape: (1, n_features)
         prediction = model.predict(input_array)  # shape: (1, 2)
         return {"x": float(prediction[0][0]), "y": float(prediction[0][1])}
